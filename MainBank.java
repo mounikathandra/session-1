a00fdbf34ed2a8de1357385f1709b7f6cb5bc688
mounikathandra <94820111+mounikathandra@users.noreply.github.com>
2022-01-03 18:54:00 +0530

    Add files via upload

---
+++ MainBank.java
@@ -0,0 +1,21 @@
+package model;
+
+	//import org.springframework.context.*;
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+	public class MainBank
+	{
+	  public static void main(String[] args)
+	  {
+		ApplicationContext context = new ClassPathXmlApplicationContext("BankContent.xml");
+		User user =(User) context.getBean(User.class);
+		System.out.println(user);
+		AccountDetails accountDetails = (AccountDetails) context.getBean(AccountDetails.class);
+		System.out.println(accountDetails);
+	  }
+	}
+
+
+
